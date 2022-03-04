e26aeac6d355061d99b2d25aee1e03e1746c9952
Toan Nguyen <nta.toan@gmail.com>
2022-03-04 13:11:04 -0500

    chore: create a loop thought dates

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
+const dimension = 7 * 5; // columns * rows
+const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
 function createDatePicker(element, fromYear, toYear) {
-  const date = new Date(fromYear, 0, 1);
-  let i = 0;
+  let counter = 1;
+  let monthEl = document.createElement('div');
+  monthEl.classList.add('month');
 
-  do {
-    date.
+  const lastDate = new Date(toYear + 1, 0, 1);
+  lastDate.setDate(-1);
 
-    date.setDate(++i);
-  } while (date.getFullYear() <= toYear);
+  for (let curr = new Date(fromYear, 0, 1); curr <= lastDate; curr.setDate(curr.getDate() + 1)) {
+    if (counter > dimension) {
+      element.append(monthEl);
+
+      monthEl = document.createElement('div');
+      monthEl.classList.add('month');
+      monthEl.dataset.month = curr.getMonth();
+      monthEl.dataset.year = curr.getFullYear();
+
+      counter = 1;
+    }
+
+    const buttonEl = document.createElement('button');
+    buttonEl.classList.add('date');
+    buttonEl.dataset.date = curr.toISOString();
+
+    if (curr.getDate() === 1) {
+      const spanEl = document.createElement('span');
+      spanEl.appendChild(document.createTextNode(monthNames[curr.getMonth()]));
+      buttonEl.append(spanEl);
+    }
+
+    const spanEl = document.createElement('span');
+    spanEl.appendChild(document.createTextNode(curr.getDate()));
+    buttonEl.append(spanEl);
+    monthEl.append(buttonEl);
+
+    counter++;
+  }
 }
